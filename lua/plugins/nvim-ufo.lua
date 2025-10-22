cb3f5e0e08d493f068076f715f21e4a42a27a6e2
Ionut Cadariu <511477+icadariu@users.noreply.github.com>
2025-10-22 16:17:57 +0200

    feat: updates to nvim ufo
    Signed-off-by: Ionut Cadariu <511477+icadariu@users.noreply.github.com>

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -2,10 +2,16 @@ return {
   { 'kevinhwang91/promise-async' },
   {
     'kevinhwang91/nvim-ufo',
-    event = 'VeryLazy',
+    event = 'BufReadPost',
     opts = {
       provider_selector = function() return { 'treesitter', 'indent' } end,
-      open_fold_hl_timeout = 100,
+    },
+    keys = {
+      { 'zR',         function() require('ufo').openAllFolds() end,  desc = 'Open all folds' },
+      { 'zM',         function() require('ufo').closeAllFolds() end, desc = 'Close all folds' },
+      -- optional leader versions so which-key shows them:
+      { '<leader>zR', function() require('ufo').openAllFolds() end,  desc = 'Open all folds' },
+      { '<leader>zM', function() require('ufo').closeAllFolds() end, desc = 'Close all folds' },
     },
   },
 }
