dc6d396b1fbe9aec36340f866c6097721ffe289c
Ionut Cadariu <511477+icadariu@users.noreply.github.com>
2025-10-22 16:44:50 +0200

    feat: updates to nvim ufo
    Signed-off-by: Ionut Cadariu <511477+icadariu@users.noreply.github.com>

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,6 +3,13 @@ return {
     'folke/which-key.nvim',
     event = 'VeryLazy',
     opts = { preset = 'modern' },
+    keys = {
+      {
+        '<leader>?',
+        function() require('which-key').show({ global = false }) end,
+        desc = 'Buffer local keymaps',
+      },
+    },
     config = function(_, opts)
       local wk = require('which-key')
       wk.setup(opts)
