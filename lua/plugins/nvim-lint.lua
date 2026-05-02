74f5a4b589b1b01c3a54032a059f4f9f74972744
Ionut Cadariu <511477+icadariu@users.noreply.github.com>
2026-05-02 21:35:04 +0200

    fix(nvim-lint): guard all linters by $PATH availability (#5)

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -4,19 +4,30 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     config = function()
       local lint = require "lint"
-      lint.linters_by_ft = {
+
+      local desired = {
         yaml = { "yamllint" },
         ansible = { "ansible_lint" },
         dockerfile = { "hadolint" },
         markdown = { "markdownlint" },
         terraform = { "tflint", "tfsec", "terraform_validate" },
         json = { "jsonlint" },
+        sh = { "shellcheck" },
+        bash = { "shellcheck" },
+        zsh = { "shellcheck" },
       }
 
-      if vim.fn.executable "shellcheck" == 1 then
-        lint.linters_by_ft.sh = { "shellcheck" }
-        lint.linters_by_ft.bash = { "shellcheck" }
-        lint.linters_by_ft.zsh = { "shellcheck" }
+      lint.linters_by_ft = {}
+      for ft, linters in pairs(desired) do
+        local present = {}
+        for _, name in ipairs(linters) do
+          if vim.fn.executable(name) == 1 then
+            table.insert(present, name)
+          end
+        end
+        if #present > 0 then
+          lint.linters_by_ft[ft] = present
+        end
       end
       vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
         callback = function()
