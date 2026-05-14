d46ef759879a8775186b4a2a5b3cd77fc84360b9
Ionut Cadariu <511477+icadariu@users.noreply.github.com>
2026-05-14 21:11:51 +0200

    feat: keep cursor at last known location
    Signed-off-by: Ionut Cadariu <511477+icadariu@users.noreply.github.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -85,6 +85,17 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
+-- Restore cursor to last known position when reopening a file
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
